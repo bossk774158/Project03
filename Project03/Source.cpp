c2c80924ec6a8d320a92a874064a64c511694b46
ASUS <ASUS@LAPTOP-CMRJTJER>
2020-08-14 19:32:29 +0700

    Add project files.

---
+++ Project03/Source.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void minmax(int n)
+{
+	int a, min = 2000000000, max = -2000000000;
+	int num[10];
+
+	for (a = 0; a < n; a++)
+		scanf("%d", &num[a]);
+
+	for (a = 0; a < n; a++)
+	{
+		if (num[a] > max)
+			max = num[a];
+
+		if (num[a] < min)
+			min = num[a];
+	}
+	printf("%d\n", min);
+	printf("%d", max);
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	minmax(n);
+	return 0;
+}
